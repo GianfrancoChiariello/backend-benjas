88f223f21634ff3faf4dd879c71e8176b6e3049a
Gianfranco Chiariello <gchiariello@pentass.com>
2023-03-13 15:03:21 -0300

    fix: :zap: Refixv2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
-const userRoutes = require('./routes/user')
+const userRoutes = require('./src/routes/user')
 const path = require('path')
 
 
@@ -29,7 +29,7 @@ const swaggerSpec = {
         ]
     },
     apis : [
-        `${path.join(__dirname, './routes/*.js')}`
+        `${path.join(__dirname, './src/routes/*.js')}`
     ]
 }
 
