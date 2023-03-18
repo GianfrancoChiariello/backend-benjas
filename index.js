d2751acbbc4334f6e7fbf75c899f71267a5900f3
Gianfranco Chiariello <gchiariello@pentass.com>
2023-03-18 16:04:56 -0300

    test(Swaggfer):
    }

--- index.js
+++ index.js
@@ -11,35 +11,6 @@ const api_key = process.env.MONGODB_KEY
 
 
 
-//Swagger config
-const swaggerUI = require('swagger-ui-express')
-const swaggerJsDoc = require('swagger-jsdoc')
-const swaggerSpec = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API de usuarios',
-            version: '1.0.0',
-            description: 'API de usuarios',
-        },
-        servers: [
-            {
-                url: 'http://localhost:9000',
-            },
-            {
-                url: 'https://backend-pink-omega.vercel.app/'
-            }
-        ]
-    },
-    apis : [
-        `${path.join(__dirname, './src/routes/*.js')}`
-    ]
-}
-
-
-
-
-
 //middleware
 app.use(express.json())
 
@@ -50,10 +21,6 @@ fs.readdirSync('./src/routes').forEach((file) => {
     app.use('/api', route)
 })
 
-//middleware for swagger
-app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(
-    swaggerJsDoc(swaggerSpec)
-))
 
 
 
